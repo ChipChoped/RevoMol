e4656155111854fa68a78649a46363d97a686c3a
Chaïmaâ Touhami <76920840+ChipChoped@users.noreply.github.com>
2025-10-31 20:46:49 +0100

    Add normalized graph edit distance calculation
    Introduces the normalized_ged function to compute the normalized graph edit distance between two molecules. Also registers NormalizedGED as a Distance instance for easier access.

--- evomol/distance/ged.py
+++ evomol/distance/ged.py
@@ -4,17 +4,33 @@ from molfeat.Classes import MoleculeFeatures
 
 def ged(molecule_1: str, molecule_2: str) -> float:
     """
-    Calculate the Levenshtein distance between two molecules.
+    Calculate the graph edition distance between two molecules.
 
     Parameters:
         molecule_1 (str): The first molecule's SMILES
         molecule_2 (str): The second molecule's SMILES
 
     Return:
-        float: The Levenshtein distance.
+        float: The graphe edition distance.
     """
 
     return MoleculeFeatures(molecule_2).evaluate_distance(MoleculeFeatures(molecule_1), "min")[0]
 
 
+def normalized_ged(molecule_1: str, molecule_2: str) -> float:
+    """
+    Calculate the graph edition distance between two molecules.
+
+    Parameters:
+        molecule_1 (str): The first molecule's SMILES
+        molecule_2 (str): The second molecule's SMILES
+
+    Return:
+        float: The graphe edition distance.
+    """
+
+    return MoleculeFeatures(molecule_2).evaluate_distance(MoleculeFeatures(molecule_1), "min")[1]
+
+
 GED = Distance("GED", ged)
+NormalizedGED = Distance("NormalizedGED", normalized_ged)
