c3efef7d764d420c20312406a0675a897ba07ee2
Chaïmaâ Touhami <76920840+ChipChoped@users.noreply.github.com>
2025-11-23 20:14:24 +0100

    Fix bad list splitting

--- scripts/attractivity.py
+++ scripts/attractivity.py
@@ -114,17 +114,11 @@ if __name__ == "__main__":
 
     print("Number of CPUs available:", max_processes)
 
+    k, m = divmod(local_optima_df.shape[0], max_processes)
+    split_rows = [local_optima_df.iloc[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(max_processes)]
+
     for n_process in range(max_processes):
-        if n_process == max_processes - 1:
-            row_stack_indexes = range(local_optima_df.shape[0] // max_processes * max_processes,
-                                      local_optima_df.shape[0])
-        else:
-            row_stack_indexes = range(local_optima_df.shape[0] // max_processes * n_process,
-                                      local_optima_df.shape[0] // max_processes * (n_process + 1))
-
-        print(len(list(row_stack_indexes)))
-
-        process: Process = Process(target=attractivity, args=(lock, local_optima_df.iloc[list(row_stack_indexes)],
+        process: Process = Process(target=attractivity, args=(lock, split_rows[n_process],
                                                               arguments.steps, path + str(steps) + ".csv", n_process))
         process.start()
         processes.append(process)
